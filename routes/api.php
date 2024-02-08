96a48a3bc21b77b8248f8437e3f6135402084697
Carlos Yuri Figueiredo <medeirosfigueiredoc@gmail.com>
2024-02-08 13:06:08 -0300

    Criando as rotas

--- routes/api.php
+++ routes/api.php
@@ -12,8 +12,14 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
-*/
+ */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiresource('cliente', App\Http\Controllers\ClienteController::class);
+Route::apiresource('carro', App\Http\Controllers\CarroController::class);
+Route::apiresource('locacao', App\Http\Controllers\LocacaoController::class);
+Route::apiresource('marca', App\Http\Controllers\Controller::class);
+Route::apiresource('cliente', App\Http\Controllers\ClienteController::class);
