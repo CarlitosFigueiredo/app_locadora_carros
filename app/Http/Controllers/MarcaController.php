501e0cf9f9c7820b3d45e56fc204f069f1b7f457
Carlos Yuri Figueiredo <medeirosfigueiredoc@gmail.com>
2024-02-21 11:25:59 -0300

    298 - Injetando a instância do Model no Controller e como lidar com o Type Hinting

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -7,12 +7,22 @@ use Illuminate\Http\Request;
 
 class MarcaController extends Controller
 {
+    public Marca $marca;
+
+    public function __construct(Marca $marca)
+    {
+        $this->marca = $marca;
+    }
+
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        $marcas = Marca::all();
+        // $marcas = Marca::all();
+        $marcas = $this->marca::all();
+
         return $marcas;
     }
 
@@ -29,15 +39,20 @@ class MarcaController extends Controller
      */
     public function store(Request $request)
     {
-        $marca = Marca::create($request->all());
+        // $marca = Marca::create($request->all());
+
+        $marca = $this->marca->create($request->all());
+
         return $marca;
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Marca $marca)
+    public function show($id)
     {
+        $marca = $this->marca->find($id);
+
         return $marca;
     }
 
@@ -52,13 +67,16 @@ class MarcaController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Marca $marca)
+    public function update(Request $request, $id)
     {
 
         // print_r($request->all()); //os dados atualizados
         // echo '<hr>';
         // print_r($marca->getAttributes()); // os dados antigos
 
+        // $marca->update($request->all());
+
+        $marca = $this->marca->find($id);
         $marca->update($request->all());
 
         return $marca;
@@ -67,9 +85,13 @@ class MarcaController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Marca $marca)
+    public function destroy($id)
     {
+        // $marca->delete();
+
+        $marca = $this->marca->find($id);
         $marca->delete();
+
         return ['msg' => 'A marca foi removida com sucesso!'];
     }
 }
